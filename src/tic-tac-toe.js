350b2fe36a84f33ff59a5fccdc4405eae601f399
Mikhail Sharakou <msharakov@gmail.com>
2019-10-03 16:58:59 +0300

    solve task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,90 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.field = [[null, null, null], [null, null, null], [null, null, null]];
+    this.currentPlayerSymbol = "x";
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (
+      this.field[rowIndex][columnIndex] === null &&
+      this.currentPlayerSymbol === "x"
+    ) {
+      this.field[rowIndex][columnIndex] = this.currentPlayerSymbol;
+      this.currentPlayerSymbol = "o";
+    } else if (
+      this.field[rowIndex][columnIndex] === null &&
+      this.currentPlayerSymbol === "o"
+    ) {
+      this.field[rowIndex][columnIndex] = this.currentPlayerSymbol;
+      this.currentPlayerSymbol = "x";
+    }
+  }
+
+  isFinished() {
+    if (this.getWinner() || this.isDraw()) {
+      return true;
     }
 
-    getCurrentPlayerSymbol() {
+    return false;
+  }
 
+  getWinner() {
+    const x = "x";
+    const o = "o";
+    const field = this.field;
+
+    if (
+      (field[0][0] === x && field[0][1] === x && field[0][2] === x) ||
+      (field[1][0] === x && field[1][1] === x && field[1][2] === x) ||
+      (field[2][0] === x && field[2][1] === x && field[2][2] === x) ||
+      (field[0][0] === x && field[1][0] === x && field[2][0] === x) ||
+      (field[0][1] === x && field[1][1] === x && field[2][1] === x) ||
+      (field[0][2] === x && field[1][2] === x && field[2][2] === x) ||
+      (field[0][0] === x && field[1][1] === x && field[2][2] === x) ||
+      (field[0][2] === x && field[1][1] === x && field[2][0] === x)
+    ) {
+      return x;
+    } else if (
+      (field[0][0] === o && field[0][1] === o && field[0][2] === o) ||
+      (field[1][0] === o && field[1][1] === o && field[1][2] === o) ||
+      (field[2][0] === o && field[2][1] === o && field[2][2] === o) ||
+      (field[0][0] === o && field[1][0] === o && field[2][0] === o) ||
+      (field[0][1] === o && field[1][1] === o && field[2][1] === o) ||
+      (field[0][2] === o && field[1][2] === o && field[2][2] === o) ||
+      (field[0][0] === o && field[1][1] === o && field[2][2] === o) ||
+      (field[0][2] === o && field[1][1] === o && field[2][0] === o)
+    ) {
+      return o;
+    } else {
+      return null;
+    }
+  }
+
+  noMoreTurns() {
+    let boolean = true;
+    this.field.forEach(item =>
+      item.forEach(item => {
+        if (item === null) boolean = false;
+      })
+    );
+    return boolean;
+  }
+
+  isDraw() {
+    if (this.noMoreTurns() && !this.getWinner()) {
+      return true;
     }
 
-    nextTurn(rowIndex, columnIndex) {
+    return false;
+  }
 
-    }
-
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
